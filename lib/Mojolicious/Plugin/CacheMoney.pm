09cfb062e9ceeeaad04c4a0f863383eab3a1d2fb
Nour Sharabash <amirite@cpan.org>
2014-04-16 01:56:40 +0000

    Import of AMIRITE/Mojolicious-Plugin-CacheMoney-0.01 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-CacheMoney
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AMIRITE/Mojolicious-Plugin-CacheMoney-0.01.tar.gz
    gitpan-cpan-author:       AMIRITE
    gitpan-cpan-maturity:     released

---
+++ lib/Mojolicious/Plugin/CacheMoney.pm
@@ -0,0 +1,80 @@
+package Mojolicious::Plugin::CacheMoney;
+use Mojo::Base 'Mojolicious::Plugin';
+
+sub register {
+    my ( $self, $app, $opts ) = @_;
+}
+
+# ABSTRACT: Bling blind
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Mojolicious::Plugin::CacheMoney - Bling blind
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+    package App;
+    use Mojo::Base 'Mojolicious';
+
+    sub startup {
+        my $self = shift;
+
+        $self->plugin( 'Mojolicious::Plugin::CacheMoney' );
+        # ...
+    }
+
+then
+
+    package App::Example;
+    use Mojo::Base 'Mojolicious::Controller';
+
+    sub test {
+        my $self = shift;
+    }
+
+=for :stopwords cpan testmatrix url annocpan anno bugtracker rt cpants kwalitee diff irc mailto metadata placeholders metacpan
+
+=head1 SUPPORT
+
+=head2 Bugs / Feature Requests
+
+Please report any bugs or feature requests through the issue tracker
+at L<https://github.com/sharabash/mojolicious-plugin-cachemoney/issues>.
+You will be notified automatically of any progress on your issue.
+
+=head2 Source Code
+
+This is open source software.  The code repository is available for
+public review and contribution under the terms of the license.
+
+L<https://github.com/sharabash/mojolicious-plugin-cachemoney>
+
+  git clone git://github.com/sharabash/mojolicious-plugin-cachemoney.git
+
+=head1 AUTHOR
+
+Nour Sharabash <amirite@cpan.org>
+
+=head1 CONTRIBUTOR
+
+Nour Sharabash <nour.sharabash@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Nour Sharabash.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
